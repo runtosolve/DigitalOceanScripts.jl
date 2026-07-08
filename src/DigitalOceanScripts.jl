67f5251a0098cef8dc935db84a3da803b6f0306d
Amoke Shabhari <amokeshabhari@gmail.com>
2026-07-08 20:29:53 +0530

    add functions with UEL

--- src/DigitalOceanScripts.jl
+++ src/DigitalOceanScripts.jl
@@ -22,6 +22,26 @@ function generate_droplet_script(remote_working_directory, model_names, local_wo
     ReadWriteFind.write_file(joinpath(local_working_directory, bash_filename), lines)
 end
 
+function generate_droplet_script_with_UEL(remote_working_directory, model_names, local_working_directory, bash_filename, uel_filename, cpus; add_queue_logic=false)
+    lines = []
+
+    push!(lines, "#!/bin/bash")
+    push!(lines, "# Generated for DigitalOcean Droplet")
+    push!(lines, @sprintf "cd %s" remote_working_directory)
+
+    if add_queue_logic
+        add_submission_to_queue!(lines)
+    end
+
+    # Abaqus execution lines
+    for i in eachindex(model_names)
+        line = "abaqus job=" * model_names[i] * " user=" * uel_filename * " cpus=$cpus" * " interactive"
+        push!(lines, line)
+    end
+
+    ReadWriteFind.write_file(joinpath(local_working_directory, bash_filename), lines)
+end
+
 function add_submission_to_queue!(lines)
     push!(lines, "")
     push!(lines, "# --- Queue logic: wait if another Abaqus job is running ---")
@@ -64,6 +84,40 @@ function deploy_to_droplet(ip_address, remote_working_directory, local_working_d
         println("Uploaded: $name.inp")
     end
     
+    println("--- Deployment Complete ---")
+end
+
+function deploy_to_droplet_with_UEL(ip_address, remote_working_directory, local_working_directory, bash_filename, uel_filename, model_names)
+    println("--- Starting Deployment to DigitalOcean Droplet ---")
+
+    #### Create remote working directory if it doesn't exist
+    check_cmd = "[ -d $remote_working_directory ] && echo 'exists' || mkdir -p $remote_working_directory"
+
+    result = read(`ssh root@$ip_address $check_cmd`, String)
+
+    if occursin("exists", result)
+        println("Directory already exists: $remote_working_directory (Skipping creation)")
+    else
+        println("Created new directory: $remote_working_directory")
+    end
+
+
+    #### Upload the .sh script
+    local_sh = joinpath(local_working_directory, bash_filename)
+    run(`scp $local_sh root@$ip_address:$remote_working_directory/`)
+
+    #### Upload the UEL Fortran file
+    local_uel = joinpath(local_working_directory, uel_filename)
+    run(`scp $local_uel root@$ip_address:$remote_working_directory/`)
+    println("Uploaded: $uel_filename")
+
+    #### Upload .inp files
+    for name in model_names
+        local_inp = joinpath(local_working_directory, "$name.inp")
+        run(`scp $local_inp root@$ip_address:$remote_working_directory/`)
+        println("Uploaded: $name.inp")
+    end
+
     println("--- Deployment Complete ---")
 end
 
